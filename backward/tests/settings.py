0b0ed3eb4cea76b0b4be6c8f6012839f626fdcda
Florent Messa <florent.messa@gmail.com>
2014-01-05 20:34:03 +0100

    Remove useless

--- backward/tests/settings.py
+++ backward/tests/settings.py
@@ -20,7 +20,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'social.apps.django_app.default',
     'backward',
     'backward.tests',
 ]
